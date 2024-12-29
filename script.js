52cfa16b33e85b5167a9d5692f3093ef9cd3c5d4
Manohar V <reddymanohar3969@gmail.com>
2024-12-30 02:45:02 +0530

    Code added for Dark and Light Mode

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ scrollToTopButton.addEventListener('click', () => {
         behavior: 'smooth' // Smooth scrolling
     });
 });
+
+const modeToggle = document.getElementById("mode-toggle");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    if (currentTheme === "dark") {
+        modeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+    }
+}
+
+modeToggle.addEventListener("click", () => {
+    const theme = document.documentElement.getAttribute("data-theme");
+    
+    if (theme === "dark") {
+        document.documentElement.setAttribute("data-theme", "light");
+        modeToggle.innerHTML = '<i class="fas fa-moon"></i>';
+        localStorage.setItem("theme", "light");
+    } else {
+        document.documentElement.setAttribute("data-theme", "dark");
+        modeToggle.innerHTML = '<i class="fas fa-sun"></i>';
+        localStorage.setItem("theme", "dark");
+    }
+});
